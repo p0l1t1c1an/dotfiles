b972384fc870155fc3360274fc34fc50c311fc00
p0l1t1c1an <boicken19@gmail.com>
2021-05-16 19:35:28 -0500

    Added openbsd dotfiles

---
+++ openbsd/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+#Path for custom scripts
+export PATH=$PATH:$HOME/bin
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar mybar &
+
